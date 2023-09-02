df199fd3ac4b043ac40bfa5429081bf86e7298e6
olimjon_rustamov <olimjonrustamov3477@gmail.com>
2023-09-02 12:26:33 +0500

    merchant query ignore case

--- src/main/java/com/example/financetest/merchant/MerchantRepository.java
+++ src/main/java/com/example/financetest/merchant/MerchantRepository.java
@@ -10,7 +10,8 @@ import java.util.List;
 public interface MerchantRepository extends JpaRepository<Merchant, Long> {
 
     @Query(value = "select * from merchant where " +
-            "merchant.name like %:query% or merchant.phone like %:query% or " +
-            "merchant.info like %:query%", nativeQuery = true)
+            "UPPER(merchant.name) like UPPER(%:query%) or " +
+            "UPPER(merchant.phone) like UPPER(%:query%) or " +
+            "UPPER(merchant.info) like UPPER(%:query%)", nativeQuery = true)
     List<Merchant> searchByQuery(String query);
 }
